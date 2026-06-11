44a06b85f79ef63be6eeedb5c3b725adef022cf4
zhaoliangjie <739144618@qq.com>
2026-06-11 19:12:46 +0800

    Initial commit: Add Spring Boot project structure with common utilities and exception handling

---
+++ src/main/java/com/zlj/aicodingstudy/AiCodingStudyApplication.java
@@ -0,0 +1,13 @@
+package com.zlj.aicodingstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodingStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCodingStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zlj/aicodingstudy/AiCodingStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlj.aicodingstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodingStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
